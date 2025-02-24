ceb65aa9556f18df9f9e38dfceab194c7cd81bad
anamarigalvez1688@gmail.com <amg1688>
2025-02-24 01:12:35 -0300

    feat: practica struct

--- StructYEnum/Program.cs
+++ StructYEnum/Program.cs
@@ -6,20 +6,25 @@ namespace StructYEnum
     {
         static void Main(string[] args)
         {
-            Empleado ana = new Empleado("Ana", 20000);
-            Console.WriteLine(ana);
-            ana.MasSalario(500);
-            Console.WriteLine(ana);
-            Empleado juan = new Empleado("Juan", 30000);
-            Console.WriteLine(juan);
-            juan = ana;
-            Console.WriteLine(juan);
-            juan.MasSalario(1200);
-            Console.WriteLine(juan);
-            Console.WriteLine(ana);
-            ana.MasSalario(100000);
-            Console.WriteLine(ana);
+            //Empleado ana = new Empleado("Ana", 20000);
+            //Console.WriteLine(ana);
+            //ana.MasSalario(500);
+            //Console.WriteLine(ana);
+            //Empleado juan = new Empleado("Juan", 30000);
+            //Console.WriteLine(juan);
+            //juan = ana;
+            //Console.WriteLine(juan);
+            //juan.MasSalario(1200);
+            //Console.WriteLine(juan);
+            //Console.WriteLine(ana);
+            //ana.MasSalario(100000);
+            //Console.WriteLine(ana);
 
+            //Coordenadas coordenada1 = new Coordenadas(32,14);
+            //coordenada1.MostrarCoordenadas();
+
+            ColorRGB colorRGB1 = new ColorRGB(250,125,72);
+            colorRGB1.MostrarColorRGB();
 
         }
     }
@@ -45,6 +50,44 @@ namespace StructYEnum
             salario+=incremento;
         }
     }
+
+    public struct Coordenadas
+    {
+        private int x;
+        private int y;
+
+        public Coordenadas(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void MostrarCoordenadas()
+        {
+            Console.WriteLine($"Coordenada x: {x}");
+            Console.WriteLine($"Coordenada y: {y}");
+        }
+    }
+
+    public struct ColorRGB
+    {
+        private byte rojo;
+        private byte verde;
+        private byte azul;
+
+        public ColorRGB(byte rojo,byte verde, byte azul)
+        {
+            this.rojo = rojo;
+            this.verde = verde;
+            this.azul = azul;
+        }
+
+        public void MostrarColorRGB()
+        {
+            Console.WriteLine($"Color RGB formado por: ({rojo},{verde},{azul})");
+        }
+    }
+
 
 
 }
